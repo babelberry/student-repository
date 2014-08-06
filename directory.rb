1122997e37d3d79f7df78c669adc11e3fb892869
babelberry <tatienantes@yahoo.fr>
2014-08-06 10:42:39 +0100

    changed student array to hash

--- directory.rb
+++ directory.rb
@@ -1,29 +1,29 @@
 #let's put all students into an array
 students = [ 
-{:name => "Javier Silverio", :cohort => :august]},
-{:name => "Elliot Lewis", :cohort => :august]}, 
-{:name => "Ben Tillett", :cohort => :august]}, 
-{:name => "Vincent Koch", :cohort => :august]},
-{:name => "Michelle Ballard", :cohort => :august]}, 
-{:name => "Nick Roberts", :cohort => :august]}, 
-{:name => "Tatiana Soukiassian", :cohort => :august]}, 
-{:name => "Mervé Silk", :cohort => :august]}, 
-{:name => "Albert Vallverdu", :cohort => :august]}, 
-{:name => "Lovis Schultze", :cohort => :august]}, 
-{:name => "Henry Stanley", :cohort => :august]}, 
-{:name => "Spike Lindsey", :cohort => :august]}, 
-{:name => "Ruth Earle", :cohort => :august]}, 
-{:name => "Andy Gates", :cohort => :august]}, 
-{:name => "Faisal Aydarus", :cohort => :august]}, 
-{:name => "Ethel Ng", :cohort => :august]}, 
-{:name => "Kevin Daniells", :cohort => :august]}, 
-{:name => "Maya Driver", :cohort => :august]}, 
-{:name => "Leopold Kwok", :cohort => :august]}, 
-{:name => "James McNeil", :cohort => :august]}, 
-{:name => "Jerome Pratt", :cohort => :august]}, 
-{:name => "David Wickes", :cohort => :august]}, 
-{:name => "Chris Oatley", :cohort => :august]}, 
-{:name => "Marc Singh", :cohort => :august]},
+{:name => "Javier Silverio", :cohort => :august},
+{:name => "Elliot Lewis", :cohort => :august}, 
+{:name => "Ben Tillett", :cohort => :august}, 
+{:name => "Vincent Koch", :cohort => :august},
+{:name => "Michelle Ballard", :cohort => :august}, 
+{:name => "Nick Roberts", :cohort => :august}, 
+{:name => "Tatiana Soukiassian", :cohort => :august}, 
+{:name => "Mervé Silk", :cohort => :august}, 
+{:name => "Albert Vallverdu", :cohort => :august}, 
+{:name => "Lovis Schultze", :cohort => :august}, 
+{:name => "Henry Stanley", :cohort => :august}, 
+{:name => "Spike Lindsey", :cohort => :august}, 
+{:name => "Ruth Earle", :cohort => :august}, 
+{:name => "Andy Gates", :cohort => :august}, 
+{:name => "Faisal Aydarus", :cohort => :august}, 
+{:name => "Ethel Ng", :cohort => :august}, 
+{:name => "Kevin Daniells", :cohort => :august}, 
+{:name => "Maya Driver", :cohort => :august}, 
+{:name => "Leopold Kwok", :cohort => :august}, 
+{:name => "James McNeil", :cohort => :august}, 
+{:name => "Jerome Pratt", :cohort => :august}, 
+{:name => "David Wickes", :cohort => :august}, 
+{:name => "Chris Oatley", :cohort => :august}, 
+{:name => "Marc Singh", :cohort => :august},
 ]
 def print_header
 	puts "The students of my cohort at Makers Academy"
@@ -31,12 +31,12 @@ def print_header
 end
 def print(students)
 students.each do |student|
-	puts "#{student[0]} (#{student[1]} cohort)"
+	puts "#{student[:name]} (#{student[:cohort]} cohort)"
 	end
 end
 	
-def print_footer(names)
-	puts "Overall we have #{students.length} great students"
+def print_footer (names)
+	puts "Overall we have #{names.length} great students"
 end
 #nothing happens until we call the methods
 print_header
