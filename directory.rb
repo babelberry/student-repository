fb98244627fc7c46ed4b406543127672741c7639
babelberry <tatienantes@yahoo.fr>
2014-08-06 11:26:27 +0100

    asked for user input

--- directory.rb
+++ directory.rb
@@ -1,30 +1,16 @@
 #let's put all students into an array
-students = [ 
-{:name => "Javier Silverio", :cohort => :august},
-{:name => "Elliot Lewis", :cohort => :august}, 
-{:name => "Ben Tillett", :cohort => :august}, 
-{:name => "Vincent Koch", :cohort => :august},
-{:name => "Michelle Ballard", :cohort => :august}, 
-{:name => "Nick Roberts", :cohort => :august}, 
-{:name => "Tatiana Soukiassian", :cohort => :august}, 
-{:name => "Mervé Silk", :cohort => :august}, 
-{:name => "Albert Vallverdu", :cohort => :august}, 
-{:name => "Lovis Schultze", :cohort => :august}, 
-{:name => "Henry Stanley", :cohort => :august}, 
-{:name => "Spike Lindsey", :cohort => :august}, 
-{:name => "Ruth Earle", :cohort => :august}, 
-{:name => "Andy Gates", :cohort => :august}, 
-{:name => "Faisal Aydarus", :cohort => :august}, 
-{:name => "Ethel Ng", :cohort => :august}, 
-{:name => "Kevin Daniells", :cohort => :august}, 
-{:name => "Maya Driver", :cohort => :august}, 
-{:name => "Leopold Kwok", :cohort => :august}, 
-{:name => "James McNeil", :cohort => :august}, 
-{:name => "Jerome Pratt", :cohort => :august}, 
-{:name => "David Wickes", :cohort => :august}, 
-{:name => "Chris Oatley", :cohort => :august}, 
-{:name => "Marc Singh", :cohort => :august},
-]
+def input_students
+	puts "Please enter the names of the students"
+	puts "To finish, just hit return twice"
+	students =[]
+	name = gets.chomp
+	while !name.empty? do 
+		students << {:name => name, :cohort => :november}
+		puts "Now we have #{students.length} students"
+		name =gets.chomp
+	end
+	students
+end
 def print_header
 	puts "The students of my cohort at Makers Academy"
 	puts "-------------------"
@@ -39,6 +25,7 @@ def print_footer (names)
 	puts "Overall we have #{names.length} great students"
 end
 #nothing happens until we call the methods
+students = input_students
 print_header
 print(students)
 print_footer(students)
