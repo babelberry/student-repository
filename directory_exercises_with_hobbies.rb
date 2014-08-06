32f2f152cf3bf58cbb92456f0fbd4db404b69723
babelberry <tatienantes@yahoo.fr>
2014-08-06 19:17:58 +0100

    exercise 6.7 center output

--- directory_exercises_with_hobbies.rb
+++ directory_exercises_with_hobbies.rb
@@ -20,11 +20,12 @@ end
 
 def add_student(name, age, country, hobby)
 		@students << {:name => name, :cohort => :august, :age => age, :country => country, :hobby => hobby}
-		puts "Now we have #{@students.length} students\n"
+		puts "Now we have #{@students.length} students"
 end
 
 def print_header
-	print "The students of my cohort at Makers Academy\n-------------------\n"
+	puts "The students of my cohort at Makers Academy".center(120)
+	puts "-------------------".center(120)
 end
 
 def print_students(students)
@@ -32,13 +33,13 @@ def print_students(students)
 	counter = 0
 	while counter < n
 		student = students[counter]
-		print "#{counter +1}. #{student[:name]} | #{student[:cohort]} cohort | #{student[:age]} years old | from #{student[:country]} | hobby : #{student[:hobby]}\n" 
+		puts "#{counter +1} #{student[:name]} | #{student[:cohort]} cohort | #{student[:age]} years old | from #{student[:country]} | hobby : #{student[:hobby]}".center(120)
 		counter += 1
 	end 
 end
 	
 def print_footer (names)
-	print "Overall we have #{names.length} great students\n"
+	puts "Overall we have #{names.length} great students"
 end
 #nothing happens until we call the methods
 students = input_students
